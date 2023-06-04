e9a98df68d4649ee4354b184304fed30510e831d
Adikwu John <jadikwu22@gmail.com>
2023-06-04 16:27:23 +0100

    =init commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const path = require('path)
+const express = require('express')
+
+const port = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('/api', (req, res)=> {
+    res.json({ message: "Hello from server!"})
+})
+
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
+app.listen(port, ()=> {
+    console.log(`Server is listening on ${[port]}`)
+})
